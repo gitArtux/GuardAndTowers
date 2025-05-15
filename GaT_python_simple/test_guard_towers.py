52599f2725cf77c96704dfa044d113d7b323726b
Ben <benutsch2000@gmail.com>
2025-05-15 17:19:29 +0200

    add more move gen tests

--- GaT_python_simple/test_guard_towers.py
+++ GaT_python_simple/test_guard_towers.py
@@ -9,9 +9,20 @@ from typing import Tuple, Dict, List, Union
 # Test cases for generate_moves: map FEN positions to expected move lists
 # FEN strings go from the top of the board to the bottom, and from left to right. As per server
 TEST_CASES_GENERATE_MOVES: Dict[str, Union[str, List[str]]] = {
+    # random state
     '3RG3/7/7/3b13/7/7/3BG3 b': 'D1-C1-1 D1-D2-1 D1-E1-1 D4-C4-1 D4-D3-1 D4-D5-1 D4-E4-1',
+    # only two guards left
     '3RG3/7/7/7/7/7/3BG3 r': 'D7-C7-1 D7-D6-1 D7-E7-1',
+    # initial state
     'r1r11RG1r1r1/2r11r12/3r13/7/3b13/2b11b12/b1b11BG1b1b1 r': 'A7-A6-1 A7-B7-1 B7-A7-1 B7-B6-1 B7-C7-1 C6-B6-1 C6-C5-1 C6-C7-1 C6-D6-1 D5-C5-1 D5-D4-1 D5-D6-1 D5-E5-1 D7-C7-1 D7-D6-1 D7-E7-1 E6-D6-1 E6-E5-1 E6-E7-1 E6-F6-1 F7-E7-1 F7-F6-1 F7-G7-1 G7-F7-1 G7-G6-1',
+    # can't capture with friendly piece in between
+    'r3RG5/r16/b16/7/7/7/3BG3 r': 'A6-A5-1 A6-A7-1 A6-B6-1 A7-A6-1 B7-B6-1 B7-C7-1',
+    # correct n=7 tower moves
+    'b76/3RG3/7/7/7/7/3BG3 b': 'A7-A1-6 A7-A2-5 A7-A3-4 A7-A4-3 A7-A5-2 A7-A6-1 A7-B7-1 A7-C7-2 A7-D7-3 A7-E7-4 A7-F7-5 A7-G7-6 D1-C1-1 D1-D2-1 D1-E1-1',
+    # blue can't capture higher red tower
+    'r43RG2/7/7/b36/7/7/3BG3 b': 'A4-A1-3 A4-A2-2 A4-A3-1 A4-A5-1 A4-A6-2 A4-B4-1 A4-C4-2 A4-D4-3 D1-C1-1 D1-D2-1 D1-E1-1',
+    # can't capture tower of hight 3 if only one field distance
+    'r62RG3/b36/7/7/7/7/3BG3 r': 'A7-B7-1 A7-C7-2 D7-C7-1 D7-D6-1 D7-E7-1',
 }
 
 class TestGenerateMoves(unittest.TestCase):
